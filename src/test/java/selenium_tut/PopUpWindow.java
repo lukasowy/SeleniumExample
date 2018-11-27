ee6ebe11695fb65a7f4b9b1d08f644a6e1041883
Lukasowy <lukasowy@gmail.com>
2018-11-27 17:36:01 +0100

    alert and windows

---
+++ src/test/java/selenium_tut/PopUpWindow.java
@@ -0,0 +1,55 @@
+package selenium_tut;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class PopUpWindow {
+
+	@Test
+	public void popUpWindow() {
+
+		System.setProperty("webdriver.gecko.driver", "webdriver/geckodriver_0.19.1-win32.exe");
+		FirefoxOptions options = new FirefoxOptions();
+		options.setCapability("marionette", false);
+
+		WebDriver driver = new FirefoxDriver(options);
+
+		driver.manage().window().maximize();
+
+		driver.get("http://demo.guru99.com/popup.php");
+
+		driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
+
+		String MainWindow = driver.getWindowHandle();
+
+		// To handle all new opened window.
+		Set<String> s1 = driver.getWindowHandles();
+		Iterator<String> i1 = s1.iterator();
+
+		while (i1.hasNext()) {
+			String ChildWindow = i1.next();
+
+			if (!MainWindow.equalsIgnoreCase(ChildWindow)) {
+
+				// Switching to Child window
+				driver.switchTo().window(ChildWindow);
+				driver.findElement(By.name("emailid")).sendKeys("gaurav.3n@gmail.com");
+
+				driver.findElement(By.name("btnLogin")).click();
+
+				// Closing the Child Window.
+				driver.close();
+			}
+		}
+		// Switching to Parent window i.e Main Window.
+		driver.switchTo().window(MainWindow);
+		driver.close();
+	}
+
+}
