ec9e64c735daeda88b08f7e5b24bdf8420f1d57f
Lukasowy <lukasowy@gmail.com>
2018-11-28 22:03:52 +0100

    new examples

--- src/test/java/selenium_tut/SeleniumDemo.java
+++ src/test/java/selenium_tut/SeleniumDemo.java
@@ -1,6 +1,10 @@
 package selenium_tut;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Set;
 
@@ -9,6 +13,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -17,6 +22,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SeleniumDemo {
@@ -25,17 +31,19 @@ public class SeleniumDemo {
 	public void test() throws Exception {
 
 		System.setProperty("webdriver.gecko.driver", "webdriver/geckodriver_0.19.1-win32.exe");
-		// FirefoxOptions options = new FirefoxOptions();
-		// options.setCapability("marionette", false);
+		FirefoxOptions options = new FirefoxOptions();
+		options.setCapability("marionette", false);
 
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new FirefoxDriver(options);
 
-		driver.get("https://www.google.com");
+		driver.get("https://www.bing.com/");
 		setMaxWindow(driver);
-
+		clickLink(driver, "Images");
 		System.out.println(getCurrentUrl(driver));
 		setWindowSize(driver, 650, 700);
-
+		getDomainName(driver);
+		// getCookies(driver);;
+		savePageSourceHTML(driver);
 		openNewWindow(driver, "https://facebook.com");
 		refreshWebpage(driver);
 		getBrowserVersion(driver);
@@ -44,11 +52,13 @@ public class SeleniumDemo {
 		Assert.assertTrue(getTitle(driver).contains("Facebook"));
 		getParentWindow(driver);
 		getAllWindows(driver);
-		takeSnapShot(driver, "D://Selenium//Screenshot//test" + Math.random() + ".png");
+		takeSnapShot(driver, "B://Selenium//Screenshot//test" + Math.random() + ".png");
 		countIFrames(driver);
 		switchToNewWindow(driver);
-		driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "n");
-		driver.close();
+		clickByClass(driver, "sc_active");
+		clickXPathLink(driver, "Privacy and Cookies");
+		// findAllLinks(driver);
+		driver.quit();
 	}
 
 	public void getAllWindows(WebDriver driver) {
@@ -108,8 +118,13 @@ public class SeleniumDemo {
 	public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
 		TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
 		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
-		File DestFile = new File(fileWithPath);
-		FileUtils.copyFile(SrcFile, DestFile);
+		try {
+			File DestFile = new File(fileWithPath);
+			FileUtils.copyFile(SrcFile, DestFile);
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
 
 	public void countIFrames(WebDriver driver) {
@@ -120,17 +135,66 @@ public class SeleniumDemo {
 	public void switchToNewWindow(WebDriver driver) {
 
 		driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "n");
-		driver.get("http://google.com");
+		driver.get("https://www.bing.com/");
 		try {
 
 			for (String winHandle : driver.getWindowHandles()) {
 				driver.switchTo().window(winHandle);
 			}
-			driver.get("http://bing.com");
+			driver.get("https://www.bing.com/");
 
 		} catch (Exception e) {
 
 		}
+	}
 
+	public void clickLink(WebDriver driver, String text) {
+		driver.findElement(By.linkText(text)).click();
+	}
+
+	public void clickByClass(WebDriver driver, String text) {
+		driver.findElement(By.className(text)).click();
+	}
+
+	public void clickXPathLink(WebDriver driver, String link) {
+		driver.findElement(By.xpath("//a[text()='" + link + "']")).click();
+	}
+
+	public void findAllLinks(WebDriver driver) {
+
+		List<WebElement> tags = driver.findElements(By.tagName("a"));
+		System.out.println(tags.size());
+		for (WebElement element : tags) {
+			System.out.println(element.getAttribute("href"));
+		}
+
+	}
+
+	public void getDomainName(WebDriver driver) throws URISyntaxException {
+		URI uri = new URI(driver.getCurrentUrl());
+		String domain = uri.getHost();
+		System.out.println(domain);
+	}
+
+	public void getCookies(WebDriver driver) {
+		Set<Cookie> cookies = driver.manage().getCookies();
+		System.out.println("Sieze of Cookiers: " + cookies.size());
+
+		for (Cookie cookie : cookies) {
+			System.out.println(cookie.getName() + " : " + cookie.getValue());
+		}
+	}
+
+	public void savePageSourceHTML(WebDriver driver) {
+		String source = driver.getPageSource();
+
+		File newFile = new File("B://Selenium//Screenshot//selenium" + Math.random() + ".txt");
+		try {
+			FileWriter fw = new FileWriter(newFile);
+			fw.write(source);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
